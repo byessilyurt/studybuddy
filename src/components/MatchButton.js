1ea20b77ed50db52fc91eae412f83b39eff05bef
byessilyurt <y.yesilyurt14@gmail.com>
2023-01-25 23:12:31 +0300

    cancel button, loading spinner, ui changes, matching status at firebase for the user

--- src/components/MatchButton.js
+++ src/components/MatchButton.js
@@ -1,8 +1,15 @@
+import React, { useState } from "react";
+
 import { motion } from "framer-motion";
 import { handleMatchButtonClick } from "../utils";
 const MatchButton = () => {
-  const handleClick = () => {
+  const [matching, setMatching] = useState(false);
+  const handleMatchClick = () => {
+    setMatching(true);
     handleMatchButtonClick();
+  };
+  const handleCancelClick = () => {
+    setMatching(false);
   };
 
   return (
@@ -10,12 +17,37 @@ const MatchButton = () => {
       <motion.button
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
-        className="rounded-full w-48 h-48 bg-indigo-500 text-white flex justify-center items-center shadow-xl hover:shadow-2xl "
-        onClick={handleClick}
+        className={`
+        disabled:pointer-events-none disabled:opacity-30 
+ rounded-full w-48 h-48 bg-indigo-500 text-white flex justify-center items-center shadow-xl hover:shadow-2xl `}
+        onClick={handleMatchClick}
+        disabled={matching}
       >
         <i className="fas fa-plus fa-2x"></i>
         <p className="text-white text-xl font-medium">Match</p>
       </motion.button>
+      {matching ? (
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="rounded-full w-24 h-24 bg-red-500 text-white flex justify-center items-center shadow-xl hover:shadow-2xl"
+          onClick={handleCancelClick}
+        >
+          <i className="fas fa-times fa-2x"></i>
+          <p className="text-white text-xl font-medium">Cancel</p>
+        </motion.button>
+      ) : null}
+      {matching ? (
+        <motion.div
+          className="loading-card text-center"
+          animate={{
+            scale: [1, 1.5, 1],
+            transition: { duration: 1.5, ease: "easeInOut", loop: Infinity },
+          }}
+        >
+          <div className="loading-text text-xl">Loading</div>
+        </motion.div>
+      ) : null}
     </div>
   );
 };
