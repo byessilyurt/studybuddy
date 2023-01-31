91915eb802b2dabc7befa31933a8560529c562c9
byessilyurt <y.yesilyurt14@gmail.com>
2023-02-01 00:01:53 +0300

    cancel btn ve loading text placed, improved animations

--- src/components/MatchButton.js
+++ src/components/MatchButton.js
@@ -6,7 +6,7 @@ const MatchButton = () => {
   const [matching, setMatching] = useState(false);
   const handleMatchClick = () => {
     setMatching(true);
-    handleMatchButtonClick();
+    //handleMatchButtonClick();
   };
   const handleCancelClick = () => {
     setMatching(false);
@@ -14,40 +14,50 @@ const MatchButton = () => {
 
   return (
     <div className="md:pl-64 flex justify-center items-start pt-8 h-screen">
-      <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className={`
-        disabled:pointer-events-none disabled:opacity-30 
- rounded-full w-48 h-48 bg-indigo-500 text-white flex justify-center items-center shadow-xl hover:shadow-2xl `}
-        onClick={handleMatchClick}
-        disabled={matching}
-      >
-        <i className="fas fa-plus fa-2x"></i>
-        <p className="text-white text-xl font-medium">Match</p>
-      </motion.button>
-      {matching ? (
+      <div className="relative">
         <motion.button
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          className="rounded-full w-24 h-24 bg-red-500 text-white flex justify-center items-center shadow-xl hover:shadow-2xl"
-          onClick={handleCancelClick}
+          className={`
+        disabled:pointer-events-none disabled:opacity-50 
+ rounded-full w-48 h-48 bg-indigo-500 text-white flex justify-center items-center shadow-2xl hover:shadow-3xl `}
+          onClick={handleMatchClick}
+          disabled={matching}
         >
-          <i className="fas fa-times fa-2x"></i>
-          <p className="text-white text-xl font-medium">Cancel</p>
+          <i className="fas fa-plus fa-2x"></i>
+          <p
+            className={`text-white text-xl font-medium ${
+              matching ? "none" : "animate-pulse"
+            }`}
+          >
+            Match
+          </p>
         </motion.button>
-      ) : null}
-      {matching ? (
-        <motion.div
-          className="loading-card text-center"
-          animate={{
-            scale: [1, 1.5, 1],
-            transition: { duration: 1.5, ease: "easeInOut", loop: Infinity },
-          }}
-        >
-          <div className="loading-text text-xl">Loading</div>
-        </motion.div>
-      ) : null}
+        {matching ? (
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="absolute -top-4 -right-8 rounded-full w-12 h-12 bg-red-500 text-white flex justify-center items-center shadow-xl hover:shadow-2xl"
+            onClick={handleCancelClick}
+          >
+            <i className="fas fa-times fa-2x"></i>
+            <p className="text-white text-xs font-medium">Cancel</p>
+          </motion.button>
+        ) : null}
+        {matching ? (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="absolute -bottom-10 left-1/3"
+          >
+            <i className="fas fa-times fa-2x"></i>
+            <p className="text-black text-md font-medium animate-pulse">
+              Loading...
+            </p>
+          </motion.div>
+        ) : null}
+      </div>
     </div>
   );
 };
