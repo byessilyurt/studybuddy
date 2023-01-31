3477bd35ba08aa5865a390d736668af816726b86
byessilyurt <y.yesilyurt14@gmail.com>
2023-02-01 00:02:30 +0300

    matched state, rendering matched if matched

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import SideBar from "./SideBar";
 import MatchButton from "./MatchButton";
 import StudyBuddyCard from "./StudyBuddyCard";
+import Matched from "./Matched";
 
 function Home() {
   const navigate = useNavigate();
+  const [matched, setMatched] = useState(true);
 
   useEffect(() => {
     let authToken = sessionStorage.getItem("Auth Token");
@@ -23,8 +25,14 @@ function Home() {
   return (
     <div>
       <SideBar />
-      <StudyBuddyCard />
-      <MatchButton />
+      {matched ? (
+        <Matched matchData={{ username: "Burak" }} />
+      ) : (
+        <>
+          <StudyBuddyCard />
+          <MatchButton />
+        </>
+      )}
     </div>
   );
 }
