00ea6d0b0fd8dd938f700d38cf7ee1a5ab05d8fb
byessilyurt <y.yesilyurt14@gmail.com>
2023-02-16 20:57:47 +0300

    functions moved to firebase folder and removed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
-import { auth, db } from "../firebase";
-import { collection, addDoc } from "firebase/firestore";
+import { auth, db, app } from "../firebase";
+import { collection, doc, getDoc, addDoc, deleteDoc } from "firebase/firestore";
 // get the current user's ID from firebase
 const getUserIdAndEmail = () => {
   const user = auth.currentUser;
@@ -12,14 +12,19 @@ const handleMatchButtonClick = async () => {
   // Get the user's ID and current timestamp
   const userInfo = getUserIdAndEmail();
   const timestamp = Date.now();
-
-  // Create a new document with the user's ID, status as "matching", and timestamp
   await addDoc(collection(db, "matching_users"), {
     userID: userInfo.userId,
     email: userInfo.email,
     status: "matching",
     timestamp: timestamp,
   });
+  console.log(userInfo.email, "has been added to the database");
 };
 
-export { handleMatchButtonClick };
+const handleCancelClick = async () => {
+  const userInfo = getUserIdAndEmail();
+  await deleteDoc(collection(db, "matching_users"), userInfo.userId);
+  console.log(userInfo.email, "has been deleted from the database");
+};
+
+export { handleMatchButtonClick, handleCancelClick };
