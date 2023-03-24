92035f9b649d15d49a23765abfa9abcdbac84fbd
byessilyurt <y.yesilyurt14@gmail.com>
2023-03-24 02:21:11 +0100

    notification added, major ui changes not complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { MatchContext } from "../context";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import SideBar from "./SideBar";
 import MatchButton from "./MatchButton";
@@ -10,23 +12,30 @@ import Matched from "./Matched";
 function Home() {
   const navigate = useNavigate();
   const { isMatched } = useContext(MatchContext);
-  const authorisedUser = localStorage.getItem("User");
+  const authorisedUser = JSON.parse(localStorage.getItem("User"));
+
+  useEffect(() => {
+    showWarningNotification();
+  }, []);
+
+  const showWarningNotification = () => {
+    toast.warning("⚠️ Don't close or switch the tab during a match!", {
+      position: "bottom-right",
+      autoClose: 10000, // 5 seconds
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  };
 
   return (
     <div>
       <SideBar />
+      <ToastContainer />
       {isMatched ? (
-        <Matched matchData={{ username: "Burak" }} />
+        <Matched />
       ) : (
         <>
-          {authorisedUser ? (
-            <h1 className="text-2xl text-center">
-              Welcome {JSON.parse(authorisedUser).email}
-            </h1>
-          ) : (
-            <h1 className="text-2xl text-center">Welcome</h1>
-          )}
-
           <StudyBuddyCard />
           <MatchButton />
         </>
--- src/components/Matched.js
+++ src/components/Matched.js
@@ -17,10 +17,39 @@ const Matched = () => {
   const { matchedUser, matchId, removeMatchedUserWithCallback } =
     useContext(MatchContext);
   const [time, setTime] = useState(0);
+  const [bgColor, setBgColor] = useState("rgba(128, 0, 128, 1)");
+
+  useEffect(() => {
+    const changeColor = () => {
+      const currentColor = bgColor.match(/\d+/g).map(Number);
+      const randomIndex = Math.floor(Math.random() * 3);
+      const randomStep =
+        (Math.random() * 0.1 - 0.05) * (Math.random() < 0.5 ? -1 : 1);
+
+      currentColor[randomIndex] = Math.min(
+        Math.max(currentColor[randomIndex] + randomStep, 0),
+        255
+      );
+
+      setBgColor(
+        `rgba(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]}, 1)`
+      );
+    };
+
+    const timer = setInterval(() => {
+      changeColor();
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [bgColor]);
+
   const handleEndMatch = async () => {
     await endMatch(matchId);
     removeMatchedUserWithCallback(() => navigate("/"));
   };
+
   useEndMatch(matchId, endMatch, removeMatchedUserWithCallback);
   useMatchChangeListener(matchId, removeMatchedUserWithCallback);
   useTimer(setTime);
@@ -29,6 +58,7 @@ const Matched = () => {
   if (!matchedUser) {
     return null;
   }
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -36,28 +66,43 @@ const Matched = () => {
       exit={{ opacity: 0 }}
       className="flex flex-col items-center p-10"
     >
-      <motion.div
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        exit={{ scale: 0 }}
-        className="bg-purple-500 rounded-full w-64 h-64 flex items-center justify-center text-white text-5xl"
-      >
-        {time} s
-      </motion.div>
-      <p className="text-2xl mt-5">You are matched with {matchedUser.email}</p>
+      <div className="flex justify-between w-full items-start">
+        <div className="flex items-center">
+          <img
+            className="w-12 h-12 rounded-full"
+            src={matchedUser.photoURL || "https://via.placeholder.com/48"}
+            alt={matchedUser.displayName}
+          />
+          <span className="ml-2 text-2xl">{matchedUser.displayName}</span>
+        </div>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          exit={{ scale: 0 }}
+          className={`rounded-full w-20 h-20 flex items-center justify-center text-white text-2xl`}
+          style={{ backgroundColor: bgColor }}
+        >
+          {time} s
+        </motion.div>
+      </div>
+      <div className="mt-5 w-full">
+        <div className="bg-gray-200 p-2 rounded-lg mb-2">
+          {/* Message box */}
+        </div>
+        <div className="bg-white p-2 rounded-lg">
+          <input
+            className="w-full p-1 focus:outline-none focus:ring-2 focus:ring-purple-500"
+            type="text"
+            placeholder="Type your message..."
+          />
+        </div>
+      </div>
       <button
         className="bg-red-600 text-white px-6 py-2 mt-4 rounded-full focus:outline-none hover:bg-red-700 transition-colors"
         onClick={handleEndMatch}
       >
         End Match
       </button>
-      <div className="mb-4 w-full mt-10">
-        <h3 className="text-lg font-medium mb-2">Chat:</h3>
-        <textarea
-          className="p-2 bg-gray-200 rounded-lg w-full h-32 focus:outline-none focus:ring-2 focus:ring-purple-500 resize-none"
-          placeholder="Start typing here..."
-        />
-      </div>
     </motion.div>
   );
 };
