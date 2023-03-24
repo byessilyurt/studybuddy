947c0a82bee2f439084760981aee590d741b4209
byessilyurt <y.yesilyurt14@gmail.com>
2023-03-24 02:21:35 +0100

    profile display updated

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,10 +9,17 @@ const SideBar = () => {
   const { matchId } = useContext(MatchContext);
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
+
+  const user = JSON.parse(localStorage.getItem("User"));
+  const userDisplayName = user ? user.displayName : "";
+  const userPhotoURL = user ? user.photoURL : "";
+  const firstName = userDisplayName.split(" ")[0];
+
   const handlelogout = async () => {
     await logout(matchId);
     navigate("/auth");
   };
+
   return (
     <>
       <button
@@ -21,29 +28,13 @@ const SideBar = () => {
       >
         <IoIosMenu />
       </button>
-      <div
-        className={`fixed inset-0 transition-all duration-500 ${
-          isOpen ? "z-20" : "-z-10"
-        }`}
-        onClick={() => setIsOpen(false)}
-      >
-        <div
-          className={`absolute z-10 inset-0 bg-gray-600 opacity-75 ${
-            isOpen ? "visible" : "invisible"
-          }`}
-        ></div>
-      </div>
+      {/* ... */}
       <div
         className={`z-40 absolute inset-y-0 left-0 w-64 transition-all duration-300 transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } md:translate-x-0 rounded-lg shadow-xl p-4 bg-slate-100`}
       >
-        <button
-          className="fixed right-2 md:invisible px-3 py-2 rounded-lg text-red-500 text-2xl hover:bg-gray-300"
-          onClick={() => setIsOpen(false)}
-        >
-          <IoIosClose />
-        </button>
+        {/* ... */}
         <nav>
           <a
             href="#"
@@ -51,21 +42,29 @@ const SideBar = () => {
           >
             Study Plans
           </a>
-          <a
-            href="#"
-            className="block p-2 w-3/4 text-indigo-500 hover:bg-indigo-100"
-          >
-            Profile
-          </a>
-          <a
-            onClick={handlelogout}
-            className="block fixed cursor-pointer w-auto text-2xl bottom-4 p-2 text-red-500 hover:bg-red-100"
-          >
-            <IoIosLogOut />
-          </a>
+          <div className="fixed bottom-4 flex justify-between w-full px-4">
+            <a
+              href="#"
+              className="flex items-center space-x-2 text-indigo-500 hover:bg-indigo-100 p-2 rounded-full"
+            >
+              <img
+                src={userPhotoURL}
+                alt={firstName}
+                className="h-8 px-2 rounded-full"
+              />
+              <span>{firstName}</span>
+            </a>
+            <a
+              onClick={handlelogout}
+              className="cursor-pointer text-2xl p-4 mx-2 text-red-500 hover:bg-red-100 rounded-full"
+            >
+              <IoIosLogOut />
+            </a>
+          </div>
         </nav>
       </div>
     </>
   );
 };
+
 export default SideBar;
